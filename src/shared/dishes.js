d112960aba198f554c118dc6f344d9c7fac2ef66
Brian Nelson <nelsonsbrian@gmail.com>
2020-01-29 22:13:58 -0800

    add onclick events to menu items

---
+++ src/shared/dishes.js
@@ -0,0 +1,39 @@
+export const DISHES = [
+  {
+    id: 0,
+    name: 'Uthapizza',
+    image: 'assets/images/uthappizza.png',
+    category: 'mains',
+    label: 'Hot',
+    price: '4.99',
+    description: 'A unique combination of Indian Uthappam (pancake) and italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Gunur chilles and Buffalo Panner.'
+  },
+  {
+    id: 1,
+    name: 'Zucchipakoda',
+    image: 'assets/images/zucchipakoda.png',
+    category: 'appetizer',
+    label: '',
+    price: '1.99',
+    description: 'Deep fried Zucchini coasted with mildy spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce.'
+  },
+  {
+    id: 3,
+    name: 'Vadonut',
+    image: 'assets/images/vadonut.png',
+    category: 'appetizer',
+    label: 'New',
+    price: '1.99',
+    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
+  },
+  {
+    id: 3,
+    name: 'ElaiCheese Cake',
+    image: 'assets/images/elaicheesecake.png',
+    category: 'dessert',
+    label: '',
+    price: '2.99',
+    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Grahm cracker crust and spiced India cardamoms.'
+  }
+
+]
